cd6d9c7b900085e3e13f3411ee09e98ad10480c4
MineDark909 <87105792+MineDark909@users.noreply.github.com>
2022-06-13 15:47:46 +0700

    Add files via upload

---
+++ background.js
@@ -0,0 +1,21 @@
+const DELAY_IN_MS = 3000;
+const VIDEO_LENGTH_IN_MS = 20000;
+
+const VIDEO_HTML_STRING =
+  "<video autoplay loop style='height: 100vh; width: 100vw'><source src='lai.mp4' type='video/mp4'></video>";
+
+function laify() {
+  setTimeout(() => {
+    const body = document.body;
+    body.innerHTML = VIDEO_HTML_STRING;
+    body.style.backgroundColor = "black";
+    setTimeout(() => {
+      window.location.replace(document.URL);
+    }, VIDEO_LENGTH_IN_MS);
+  }, DELAY_IN_MS);
+}
+
+const chance = Math.floor(Math.random() * 100);
+if (chance <= 10) {
+  laify();
+}
